ba9e512cf17c306ecdcfd59dcc964020b9cca8ff
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-04-21 21:14:08 -0600

    Add files via upload

---
+++ push_button_print/push_button_print.ino
@@ -0,0 +1,27 @@
+// Set pin numbers
+
+const int buttonPin = 4;
+
+int buttonState = 0;      
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);   // Read the state of the pushbutton value
+
+  if (buttonState == HIGH) {
+    Serial.println("何振铭");
+  }
+  else {
+    
+  }
+
+}
